3a79673c13a6edfb6894cd863f712fcf116fd124
Allan Joshua <calla@amazon.com>
2019-06-22 02:47:53 -0700

    Support possibilities in utils

--- src/models/utils.js
+++ src/models/utils.js
@@ -4,6 +4,7 @@ import getSymbolFromCurrency from 'currency-symbol-map';
 
 export default class Utils {
   getUnits = () => convert().possibilities();
+  getUnitsPossibilitiesFrom = (from) => convert().from(from).possibilities();
   getCurrencyCodes = () => Array.from(new Set(Object.keys(currencyToSymbolMap)));
   getCurrencySymbol = currencyCode => getSymbolFromCurrency(currencyCode);
 }
