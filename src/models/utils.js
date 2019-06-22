685926188065e5791c7684d21e9fb397978006ef
Allan Joshua <calla@amazon.com>
2019-06-22 03:08:24 -0700

    Reduce unit support to length, mass, area and volume

--- src/models/utils.js
+++ src/models/utils.js
@@ -2,9 +2,12 @@ import convert from 'convert-units';
 import currencyToSymbolMap from 'currency-symbol-map/map';
 import getSymbolFromCurrency from 'currency-symbol-map';
 
+const measures = ['length', 'area', 'mass', 'volume'];
+
 export default class Utils {
-  getUnits = () => convert().possibilities();
-  getUnitsPossibilitiesFrom = (from) => convert().from(from).possibilities();
+  getUnits = (from) => from ? convert().from(from).possibilities() : convert().list()
+    .filter(({ measure }) => measures.indexOf(measure) > -1)
+    .map(({ abbr }) => abbr);
   getCurrencyCodes = () => Array.from(new Set(Object.keys(currencyToSymbolMap)));
   getCurrencySymbol = currencyCode => getSymbolFromCurrency(currencyCode);
 }
