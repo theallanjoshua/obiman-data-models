8c79101f80fff32d2cda10fe8903fe49bc1c1d06
Allan Joshua <calla@amazon.com>
2019-06-28 12:49:25 +0530

    Added bill to exports

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 export { default as Ingredient } from './models/ingredient';
 export { default as Product } from './models/product';
 export { default as ProductCompositionEntity } from './models/product-composition-entity';
+export { default as Bill } from './models/bill';
+export { default as BillCompositionEntity } from './models/bill-composition-entity';
 export { default as Response } from './models/response';
 export { default as Utils } from './models/utils';
