8fdbdb77e3b04b24b7380f35960503a483dfa396
Allan Joshua <calla@amazon.com>
2019-11-03 13:49:27 +0530

    Fix metadata validation issue

--- src/models/business.js
+++ src/models/business.js
@@ -107,7 +107,8 @@ export default class Business {
       const metadataItems = this.metadata[key];
       const emptyMetadataItemsErrors = metadataItems.filter(item => !item).length ? [ 'Cannot have empty values' ] : [];
       const duplicateMetadataItemsErrors = metadataItems.filter(({ id }, index, array) => array.filter(employee => employee.id === id).length > 1).length ? [ 'Some values are used more than once' ]: [];
-      return errors ? { ...acc, [key]: [ ...emptyMetadataItemsErrors, ...duplicateMetadataItemsErrors ] } : { ...acc };
+      const errors = [ ...emptyMetadataItemsErrors, duplicateMetadataItemsErrors ];
+      return errors ? { ...acc, [key]: errors } : { ...acc };
     }, {});
     return { ...labelErrors, ...currencyErrors, ...employeesErrors, ...contactErrors, ...(Object.keys(metadataErrors).length ? { metadata: { ...metadataErrors } } : {}) };
   }
